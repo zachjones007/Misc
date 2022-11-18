fa8d1f78b9128b15281109ef9b8fc469642f9cf1
zach jones <zach@jonesaz.com.com>
2022-11-18 06:49:08 -0700

    need to add these codes together

--- green.py
+++ green.py
@@ -1,20 +1,56 @@
 
-year1 = [] 
-year2 = [] 
-months = ["Jan","Feb","Mar","Apr","May","Jun","jul","aug","sep","Oct","Nov","Dec"]
+year1 = []
+year2 = []
+months = ["Jan","Feb"]#,"Mar","Apr","May","Jun","jul","aug","sep","Oct","Nov","Dec"]
 while True:
-        print("year 1")
-        year1 = [float(input("how many bottles today?: ")) for b in range(12)]
-        print("year 2")
-        year2 = [float(input("how many bottles today?: ")) for b in range(12)]
+    for i in range( len(months)):
+        print("Enter the number of green beans harvested in", months[i], "for year 1")
+        year1.append(int(input()))
+        print("Enter the number of green beans harvested in", months[i], "for year 2")
+        year2.append(int(input()))
+    print(year1)    
+    print(year2)
         
         
-        savings = list()
-        for list1, list2 in zip(year1, year2):
-            savings.append(list1 - list2)
-  
-  
-        option = str(input('would you like to contiune? yes or quit '))
-        for letter in option:
-             if letter in "n,N,q,Q":
-                    quit("quit")
+    savings = list()
+    for list1, list2 in zip(year1, year2):
+            print(savings + months)
+
+    print(*months, sep = "\n")
+    break
+
+#++++ 
+#++++
+#++++
+#++++
+#++++ 
+
+
+sentence = "WHAT IS MINE IS YOURS AND WHAT IS YOURS IS MINE";
+
+sentence = sentence.lower();
+
+sentence = sentence.split();
+
+uniqueWord = [];
+
+store = [];
+
+for i in sentence:
+    if i not in uniqueWord:
+        uniqueWord.append(i);
+
+lengthOfUniqueWord = len(uniqueWord);
+
+print(sentence);
+
+print(uniqueWord);
+
+for i in range(lengthOfUniqueWord):
+    i = str(i+1);
+    store.append(i);
+
+print(store);
+
+for positions in enumerate(uniqueWord, 1):
+     print(positions);
