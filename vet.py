b89e77ed6db2761206dc4201811d0db1a5b96cba
zach jones <zach@jonesaz.com.com>
2022-11-23 16:28:03 -0700

    redo

--- vet.py
+++ vet.py
@@ -1,79 +1,33 @@
-class Employee:
-    def __init__(self): #Constructor
-        self.__id = 0
-        self.__name = ""
-        self.__gender = ""
-        self.__city = ""
-        self.__salary = 0
 
-    @property
-    def id(self):
-        return self.__id
+  
+class Pet:
+    def __init__(self, age = 0,name = '',type = ''):
+         self._age = age
+         self._name = name
+         self._type = type
+      
+    # getter method
+    def get_age(self):
+        return self._age
+      
+    # setter method
+    def set_age(self, x):
+        self._age = x
 
-    @id.setter
-    def id(self,value):
-        self.__id=value
+    def set_name(self):
+        return self._name
 
-    @property
-    def name(self):
-        return self.__name
+    def get_name(self,y):
+        self._name = y
 
-    @name.setter
-    def name(self, value):
-        self.__name = value
-
-    @property
-    def gender(self):
-        return self.__gender
-
-    @gender.setter
-    def gender(self, value):
-        self.__gender = value
-
-    @property
-    def city(self):
-        return self.__city
-
-    @city.setter
-    def city(self, value):
-        self.__city = value
-
-    @property
-    def salary(self):
-        return self.__salary
-
-    @salary.setter
-    def salary(self, value):
-        self.__salary = value
-
-def main():
-    print("Enter Employee Data:")
-    i = int(input("Enter Id\t:"))
-    n = input("Enter Name\t:")
-    g = input("Enter Gender:")
-    c = input("Enter City\t:")
-    s = int(input("Enter Salary:"))
-
-    e=Employee()
-    e.id=i
-    e.name=n
-    e.gender=g
-    e.city=c
-    e.salary=s
-    id2 = e.id
-    name2 = e.name
-    gender2 = e.gender
-    city2 = e.city
-    salary2 = e.salary
-
-
-    print("\nDisplaying Employee Data:")
-    print("Id\t\t:", id2)
-    print("Name\t:", name2)
-    print("Gender\t:", gender2)
-    print("City\t:", city2)
-    print("Salary\t:", salary2)
-
-
-if __name__=="__main__":
-    main()
+    
+  
+p = Pet()
+  
+# setting the age using setter
+p.set_age(21)
+  
+# retrieving age using getter
+print(p.get_age())
+  
+print(p._age)
