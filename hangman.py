2b51a02c13a5c8d6be493a1a8a56a87b83427713
zach jones <zach@jonesaz.com.com>
2022-11-24 07:00:03 -0700

    `

--- hangman.py
+++ hangman.py
@@ -5,36 +5,36 @@ print("Original list is : " + str(test_list))
 cpu_choice =[]
 cpu_choice=("Random element is :", random.sample(test_list, 1)) 
 number=(len(cpu_choice))
+print(number)
 
 
 if 4 <= len(cpu_choice):
   print (number)
 x = 0
 y = 0
-while x < 5:
- 
-  while y < number:
-  #numbers = 2 
-  
-  
-  
+match = False 
+while x < 5 or y < number:
+
     userinput = input('guess a letter: ')[0]
-    match = False
+    
+
+    for letter in cpu_choice[1][0]:
+      if letter != userinput:
+        x = x +1 
     for letter in cpu_choice[1][0]:
       if letter == userinput:
         match = True
         y = y +1 
-        # add one to y to make it equal to len of cpu choice
-        
 
-        if len(cpu_choice) == y:
-          quit('quit')
-    if match:
-      print('correct')
-      y=y+1
-    else:
-      print('wrong')
-      x = x + 1
-print("end")
+      
+    print(cpu_choice)
+    if number == y:
+        print('you win')
+        exit()
+
+        
+     
 
     
+#++++++++++++
+ 
