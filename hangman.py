7eb44c7507351991d8ea0f80af113ac8619d430f
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2022-11-23 21:09:26 -0700

    fix

--- hangman.py
+++ hangman.py
@@ -38,3 +38,25 @@ while x < 5:
 print("end")
 
     
+#++++++++++++
+
+
+import random
+test_list = [ 'yes', 'no']
+# guessing list
+print("Original list is : " + str(test_list))
+cpu_choice =[]
+cpu_choice=("Random element is :", random.sample(test_list, 1)) 
+print (cpu_choice) 
+
+userinput = input('guess a letter: ')[0]
+match = False
+for letter in cpu_choice[1][0]:
+  if letter == userinput:
+    match = True
+    break
+if match:
+  print('correct')
+else:
+  print('wrong')
+
