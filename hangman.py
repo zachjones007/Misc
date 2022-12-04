4f4d1d2dfac3b14e0dc93c7684ab437111da208b
zach jones <zach@jonesaz.com.com>
2022-12-04 07:12:22 -0700

    trying new things

--- hangman.py
+++ hangman.py
@@ -1,73 +1,52 @@
-
-
-
-
-
-
 import random
-test_list = [ 'yes', 'no']
-# guessing list
-print("Original list is : " + str(test_list))
-cpu_choice =[]
-cpu_choice=("Random element is :", random.sample(test_list, 1)) 
-number=(len(cpu_choice))
-print(number)
 
 
-
-x = 0
-y = 0
-match = False 
-
-while x < 5:
-  
-  
-  
-  
-    userinput = input('guess a letter: ')[0]
-    match = False
-    
-    for letter in cpu_choice[1][0]:
-      if letter == userinput:
-        match = True
-  
+def megamain():
+    userinput = []
+    test_list = [ 'yes', 'no']
+    # guessing list
+    print("Original list is : " + str(test_list))
+    cpu_choice =[]
+    cpu_choice=("Random element is :", random.sample(test_list, 1)) 
+    number=(len(cpu_choice))
+    print(number)
+    x = 0
+    y = 0
+    match = False 
+    while  x < 5:
         
-        if len(cpu_choice) == y:
-          quit('quit')
-    if match:
-      print('correct')
-      y=y+1
-    else:
-      print('wrong')
-      x = x + 1
-print("end")
-
-
-#++++ top code x works bottom code y works
-while x < 5:
-
-    userinput = input('guess a letter: ')[0]
-    if y < number:
-      quit
-    
-
-  
-      
-    for letter in cpu_choice[1][0]:
-      if letter == userinput:
-        match = True
-        y = y +1 
-        print("correct")
-
-      elif x + 1:
-        print ('incorrect')
-        match = False 
-    print(cpu_choice)
-    if number == y:
-        print('you win')
-        exit()
-
         
-     
+
+        userinput = input('guess a letter: ')[0]
+        with open('hangman.txt', 'a') as f:
+                    f.write(str(userinput))
+                    f.writelines("/  \n".format(userinput))
+                   
+        match = False
+        
+        for letter in cpu_choice[1][0]:
+         if letter == userinput:
+            match = True
+    
+            
+            if len(cpu_choice) == y:
+             quit('quit')
+        if match:
+            print('correct')
+            y=y+1
+        
+        else:
+            print('wrong')
+            x = x + 1
+            print(userinput)
+        
+        
+            
+
+       
+megamain()
+
+
+
 
     
